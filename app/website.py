3f589343f200bf9b832f0e03f8a32f01fe36a68e
frangiz <frangiz@gmail.com>
2021-08-14 13:40:07 +0200

    Some minor changes in the result from the index page.

--- app/website.py
+++ app/website.py
@@ -1,12 +1,8 @@
 from flask import Blueprint
-from app.models import PricedProduct
-from flask.json import jsonify
+from app.api import list_priced_products
 
 bp = Blueprint("website", __name__)
 
 @bp.route("/", methods=["GET"])
 def index():
-    entries = PricedProduct.query.all()
-    res = [e.to_dict() for e in entries]
-
-    return jsonify(res), 200
+    return list_priced_products()
