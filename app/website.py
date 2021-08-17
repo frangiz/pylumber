5574d871a6470b16c73d4e1a52d8ad65fd0d17f0
frangiz <frangiz@gmail.com>
2021-08-17 23:52:55 +0200

    Added jinja2 templates and chart.js. Readability is really low. Not sure how to make it better now, but hey, it works.

--- app/website.py
+++ app/website.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
-from app.api import list_priced_products
+from flask import Blueprint, render_template
+from app.models import PricedProduct, PriceSnapshot
+from itertools import groupby
+from operator import attrgetter
 
 bp = Blueprint("website", __name__)
 
 @bp.route("/", methods=["GET"])
 def index():
-    return list_priced_products()
+    entries = PricedProduct.query.all()
+
+    res = [{"group_name": k, "sources": [e.to_dict_except(["group_name"]) for e in g]} for k, g in groupby(entries, attrgetter("group_name"))]
+
+    for group in res:
+        for source in group["sources"]:
+            price_snapshots = PriceSnapshot.query.filter_by(priced_product_id=source["id"]).all()
+            source["prices"] = [ps.to_dict_except(["id", "priced_product_id"]) for ps in price_snapshots]
+            del source["id"]
+
+    return render_template("main.html", groups=res)
