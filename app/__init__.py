42ebb9c7e67f5be6a6c417b8016fa06d9c3700f4
frangiz <frangiz@gmail.com>
2022-05-09 13:56:37 +0200

    Added environment config for Sentry, will be same as FLASK_ENV.

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,8 @@ def create_app(config_class=Config):
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
-        traces_sample_rate=1.0
+        traces_sample_rate=1.0,
+        environment=app.config['FLASK_ENV']
     )
 
     # setup logging
--- config.py
+++ config.py
@@ -8,4 +8,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "pylumber.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+
     SENTRY_SDK_DSN = os.environ.get('SENTRY_SDK_DSN')
